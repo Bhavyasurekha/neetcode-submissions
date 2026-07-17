d062ad7da25d6e2a59b2ffc552d6c6dccdc876de
Bhavyasurekha <107043593+Bhavyasurekha@users.noreply.github.com>
2026-07-17 14:57:53 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // for(int i=0;i<nums.length;i++){
+        // //     for(int j=i+1;j<nums.length;j++){
+        // //         if(nums[i]==nums[j]){
+        // //             return true;
+        // //         }
+        // //     }
+        // // }
+        // // return false ;
+        // ----------------------------------
+        // if(nums.length<=1){
+        //     return false;
+        // }
+        // Arrays.sort(nums);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        // ------------------------------------------
+        if(nums.length<=1){
+            return false;
+        }
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!hs.contains(nums[i])){
+                hs.add(nums[i]);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
